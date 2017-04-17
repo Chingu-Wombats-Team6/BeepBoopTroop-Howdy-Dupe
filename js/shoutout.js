b69ca433c2fccc2fc7e8c6465b34c07d2c3afcaf
Tim Mondell <timmondellgeneral@gmail.com>
2017-04-17 11:31:01 -0700

    went back to original location in computer for git

--- js/shoutout.js
+++ js/shoutout.js
@@ -1,6 +1,8 @@
-$(document).ready(function(){
-
+/* ORIGINAL SOMEWHAT WORKING 
+$(document).ready(function(){ 
+	$('.howdy-main .shoutout .dots').show().delay(3000).fadeOut();
 	$('#shoutout > div:gt(0)').hide();
+
 
     setInterval(function() {
 	    $('#shoutout > div:first')
@@ -28,31 +30,35 @@ $(document).ready(function(){
       });
   });
 
+ORIGINAL SOMEWHAT WORKING */
+
+$(document).ready(function(){ 
+	$('#shoutout > div:gt(0)').hide();
+	$('#dots').show(0).delay(1800).hide(0);
 
 
-/* var shoutout;
-shoutout = document.getElementById("shoutout-lego");
-console.log(shoutout)
-for (var i = 0; i< shoutout.length; ++i) {
-	shoutout[i].classList.add('visible')
-}
-*/
+    setInterval(function() {
+	    $('#shoutout > div:first')
+	    .fadeOut(0)
+	    .next()
+	    .fadeIn(0)
+	    .end()
+	    .appendTo('#shoutout');
+	},  9000);
 
 
-/* var x = document.querySelectorAll("div.shoutout");
 
-x[0].display="block";
-x[1].display=block;
-x[2].display=none; */
-
-//Array.from(myNodeList).forEach(function(el) { console.log(el); 
-// });
-
-/*
-$(document).ready(function(){
-	$('.shoutout').show();
-	setTimeout(function(){
-		$('.shoutout').hide();
-	},5000);
-})
-*/
+    $('#shoutout-lego').hide();
+    $('#shoutout-lego')
+     .delay(1800)
+     .queue( function(next){
+		 $(this).show();
+		 next();
+     });
+     $('.highlights1')
+      .delay(1800)
+      .queue( function(next){
+        $(this).hide();
+        next();
+      });
+  });
